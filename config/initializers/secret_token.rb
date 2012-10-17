1f06d49a93763a5aece68b9264a5a7533d859666
CarlescuEcaterina <carlescuecaterina@student.(none)>
2012-10-17 19:28:25 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcaterinaCarlescu::Application.config.secret_token = '68e0859b66d065409b9251bc3ee4c4051ea03d08421aeada8855ae4c9d6ad8e0bb04e5da7e92b6b08b2e2ca0e2d9234ab4e544d9177d22507de2b89521b8f749'
