b988cc338593d8e2ce9f68b6de38f6cc980f71b8
CarlescuEcaterina <carlescuecaterina@student.(none)>
2012-11-10 13:45:43 +0200

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+  before_save {|user| user.email = email.downcase}
   has_one :CV, :foreign_key=>'user_id'
+
+  validates :first_name, :presence => true, length: { maximum: 25}
+  validates :last_name, :presence => true, length: {maximum: 25}
+  Valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+			format: {with: Valid_email },.
+			uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 8}
+  validates :password_confirmation, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121031155657) do
+ActiveRecord::Schema.define(:version => 20121110110622) do
 
   create_table "companies", :force => true do |t|
     t.integer  "id_company"
@@ -56,8 +56,11 @@ ActiveRecord::Schema.define(:version => 20121031155657) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
